0c7adbbd3fa1ceb7f39fef6c063ee7c22279e424
MaratMir <Marat.Mirgaleev@autodesk.com>
2014-01-16 11:47:19 +0400

    CFilesOnDisk - unit test and comments

--- MArcLib/CFilesOnDisk.h
+++ MArcLib/CFilesOnDisk.h
@@ -1,29 +1,26 @@
 // DiskArcher.
 // CFilesOnDisk class - a list of files.
-// (C) Marat Mirgaleev, 2004.
+// (C) Marat Mirgaleev, 2014.
 // Created 11.07.2004.
-// Modifications:
-//================================================================================
+// Note: Do not add objects created on stack - it leads to an exception in the collection destruction.
+//====================================================================================================
 
-#if !defined( CFILESONDISK_H__INCLUDED)
-#define CFILESONDISK_H__INCLUDED
-
-#if _MSC_VER > 1000
 #pragma once
-#endif // _MSC_VER > 1000
 
 #include <afxtempl.h>
 #include "CFileOnDisk.h"
 
 
 class CFilesOnDisk : public CList< CFileOnDisk*, CFileOnDisk* >
+  // Yeah, I know that inheritance from containers has some drawbacks, but in this case that's fine
 {
 public:
-    ~CFilesOnDisk();
+    ~CFilesOnDisk(); // I'd make it virtual, but the base CList destructor is not virtual
 
     POSITION FindPos( CString sPath, CString sName ) const;
+
+    // sPath shall end with a slash
     CFileOnDisk* Find( CString sPath, CString sName ) const;
+
     unsigned int Remove( CString sPath, CString sName );
 };
-
-#endif // !defined(CFILESONDISK_H__INCLUDED)
--- MArcLibUnitTest/UnitTest.cpp
+++ MArcLibUnitTest/UnitTest.cpp
@@ -2,20 +2,40 @@
 #include "CppUnitTest.h"
 
 #include "CDrive.h"
+#include "CFileOnDisk.h"
+#include "CFilesOnDisk.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace MArcLibUnitTest
 {
-  TEST_CLASS(MArcUnitTest)
+  TEST_CLASS(MArcLibUnitTest)
   {
   public:
     
     TEST_METHOD(CDrive_TestMethod)
     {
       CDrive drive("C:");
-      Logger::WriteMessage(drive.m_strName);
+      Logger::WriteMessage( "CDrive_TestMethod: " + drive.m_strName );
       Assert::AreEqual(drive.m_nDriveType, (UINT)DRIVE_FIXED);
+    }
+
+
+    TEST_METHOD(CFilesOnDisk_TestMethod)
+    {
+      CFilesOnDisk files;
+      Logger::WriteMessage( "CFilesOnDisk_TestMethod" );
+
+      // !!! CFileOnDisk aFile( "X:\\ZZZZ\\YYY.UUU" );
+      //   Creating an object on stack leads to an exception in the collection destruction
+      CFileOnDisk* aFile = new CFileOnDisk( "X:\\ZZZZ\\YYY.UUU" );
+      files.AddTail( aFile );
+      CFileOnDisk* found = files.Find( "X:\\ZZZZ\\", "YYY.UUU" );
+
+      // VS test somehow can't compare two CFileOnDisk*, let's cast to void* then
+      void* ptr1 = aFile;
+      void* ptr2 = found;
+      Assert::AreEqual( ptr1, ptr2 );
     }
 
   };
