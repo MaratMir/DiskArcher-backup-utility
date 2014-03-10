107713307daa5b64eaabf1a79d3ec4584dce48d3
MaratMir <Marat.Mirgaleev@autodesk.com>
2014-03-10 12:31:42 +0400

    Tabs to spaces
    no any meaningful changes

--- CRoomsView.cpp
+++ CRoomsView.cpp
@@ -1,13 +1,13 @@
 // DiskArcher.
-// Implementation of the CRoomsView class.
-// (C) Marat Mirgaleev, 2001-2002.
+// CRoomsView class - shows Rooms.
+// (C) Marat Mirgaleev, 2001-2014.
 // Modifications:
-//	(1) 09.02.2002. GetSelectedRoom() added.
-//	(2) 26.05.2002. SetSubItems() correction.
-//	(3) 01.11.2002. theDB changed to *pTheDB.
-//	(4) 16.01.2003. EraseList() moved into the base class.
-//	(5) 11.05.2003. Properties dialog changes.
-//	(6) 28.10.2003. Compression mode.
+//  (1) 09.02.2002. GetSelectedRoom() added.
+//  (2) 26.05.2002. SetSubItems() correction.
+//  (3) 01.11.2002. theDB changed to *pTheDB.
+//  (4) 16.01.2003. EraseList() moved into the base class.
+//  (5) 11.05.2003. Properties dialog changes.
+//  (6) 28.10.2003. Compression mode.
 //=====================================================================
 
 #include "stdafx.h"
@@ -17,10 +17,10 @@
 #include "CRoomsView.h"
 
 #include "MArcCore/CRoom.h"
-#include "MArcCore/CMyArchive.h" // M
-#include "MArcCore/CArchiveDB.h" // M
-#include "MArcCore/CFileToArc.h" // M
-#include "CRoomPropDialog.h" // M
+#include "MArcCore/CMyArchive.h"
+#include "MArcCore/CArchiveDB.h"
+#include "MArcCore/CFileToArc.h"
+#include "CRoomPropDialog.h"
 #include "MainFrm.h"
 
 #ifdef _DEBUG
@@ -29,185 +29,169 @@
 static char THIS_FILE[] = __FILE__;
 #endif
 
-/////////////////////////////////////////////////////////////////////////////
-// CRoomsView
 
 IMPLEMENT_DYNCREATE(CRoomsView, CListView)
 
 BEGIN_MESSAGE_MAP(CRoomsView, CListView)
-	//{{AFX_MSG_MAP(CRoomsView)
-	ON_WM_CREATE()
-	ON_COMMAND(ID_ROOM_PROPERTIES, OnRoomProperties)
-	ON_COMMAND(ID_ROOM_NEW, OnRoomNew)
-	ON_UPDATE_COMMAND_UI(ID_ROOM_PROPERTIES, OnUpdateRoomProperties)
-	ON_NOTIFY_REFLECT(LVN_KEYDOWN, OnKeyDown)
-	ON_COMMAND(ID_ROOM_DELETE, OnRoomDelete)
-	ON_UPDATE_COMMAND_UI(ID_ROOM_DELETE, OnUpdateRoomDelete)
-	//}}AFX_MSG_MAP
-	ON_NOTIFY_REFLECT(NM_RCLICK, OnRightClick)
+  //{{AFX_MSG_MAP(CRoomsView)
+  ON_WM_CREATE()
+  ON_COMMAND(ID_ROOM_PROPERTIES, OnRoomProperties)
+  ON_COMMAND(ID_ROOM_NEW, OnRoomNew)
+  ON_UPDATE_COMMAND_UI(ID_ROOM_PROPERTIES, OnUpdateRoomProperties)
+  ON_NOTIFY_REFLECT(LVN_KEYDOWN, OnKeyDown)
+  ON_COMMAND(ID_ROOM_DELETE, OnRoomDelete)
+  ON_UPDATE_COMMAND_UI(ID_ROOM_DELETE, OnUpdateRoomDelete)
+  //}}AFX_MSG_MAP
+  ON_NOTIFY_REFLECT(NM_RCLICK, OnRightClick)
 END_MESSAGE_MAP()
 
 
 enum { imgUnavailable, imgOk, imgRemovable };
 
 
-/////////////////////////////////////////////////////////////////////////////
-// CRoomsView construction/destruction
-
 CRoomsView::CRoomsView()
 {
-	m_nIDColumn = 0;	// TO DO: Change when columns dragged and dropped
-}
-
-
-CRoomsView::~CRoomsView()
-{
+  m_nIDColumn = 0; // TODO: Change when columns dragged and dropped
 }
 
 
 BOOL CRoomsView::PreCreateWindow(CREATESTRUCT& cs)
 {
-	// TODO: Modify the Window class or styles here by modifying
-	//  the CREATESTRUCT cs
+  // TODO: Modify the Window class or styles here by modifying
+  //  the CREATESTRUCT cs
 
-	return CListView::PreCreateWindow(cs);
+  return CListView::PreCreateWindow(cs);
 }
 
-/////////////////////////////////////////////////////////////////////////////
-// CRoomsView drawing
 
 void CRoomsView::OnDraw(CDC* pDC)
 {
-	UNREFERENCED_PARAMETER( pDC );
-	CRoomsDoc* pDoc = GetDocument();
-	ASSERT_VALID(pDoc);
-	// TODO: add draw code for native data here
+  UNREFERENCED_PARAMETER( pDC );
+  CRoomsDoc* pDoc = GetDocument();
+  ASSERT_VALID(pDoc);
+  // TODO: add draw code for native data here
 }
+
 
 void CRoomsView::OnInitialUpdate()
 {
-	CListView::OnInitialUpdate();
+  CListView::OnInitialUpdate();
 
-	// TODO: You may populate your ListView with items by directly accessing
-	//  its list control through a call to GetListCtrl().
-	ShowRoomList();
+  // TODO: You may populate your ListView with items by directly accessing
+  //  its list control through a call to GetListCtrl().
+  ShowRoomList();
 }
 
-/////////////////////////////////////////////////////////////////////////////
-// CRoomsView diagnostics
 
 #ifdef _DEBUG
 void CRoomsView::AssertValid() const
 {
-	CListView::AssertValid();
+  CListView::AssertValid();
 }
 
 void CRoomsView::Dump(CDumpContext& dc) const
 {
-	CListView::Dump(dc);
+  CListView::Dump(dc);
 }
 
 CRoomsDoc* CRoomsView::GetDocument() // non-debug version is inline
 {
-	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CRoomsDoc)));
-	return (CRoomsDoc*)m_pDocument;
+  ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CRoomsDoc)));
+  return (CRoomsDoc*)m_pDocument;
 }
 #endif //_DEBUG
 
 
-/////////////////////////////////////////////////////////////////////////////
-// CRoomsView message handlers
-
 void CRoomsView::OnRightClick(NMHDR *pNotifyStruct, LRESULT *result)
 {
-	MyOnRightClick( pNotifyStruct );
-	*result = 0;
+  MyOnRightClick( pNotifyStruct );
+  *result = 0;
 }
 
 
 int CRoomsView::OnCreate(LPCREATESTRUCT lpCreateStruct) 
 {
-	lpCreateStruct->style |= LVS_REPORT | /*LVS_NOSORTHEADER |*/ LVS_SINGLESEL;
-//	lpCreateStruct->dwExStyle |= ; // Это не работает, а работает SetExtendedStyle
-	if (CListView::OnCreate(lpCreateStruct) == -1)
-		return -1;
-	
+  lpCreateStruct->style |= LVS_REPORT | /*LVS_NOSORTHEADER |*/ LVS_SINGLESEL;
+// lpCreateStruct->dwExStyle |= ; // It doesn't work this way, it works via SetExtendedStyle
+  if (CListView::OnCreate(lpCreateStruct) == -1)
+    return -1;
+  
 // Give the document a pointer to this view
-	GetDocument()->m_pListView = this;
-	
-	CListCtrl& ctlList = (CListCtrl&) GetListCtrl();
-	ctlList.SetExtendedStyle( LVS_EX_HEADERDRAGDROP | LVS_EX_FULLROWSELECT | 
-							  LVS_EX_GRIDLINES | LVS_EX_SUBITEMIMAGES );
+  GetDocument()->m_pListView = this;
+  
+  CListCtrl& ctlList = (CListCtrl&) GetListCtrl();
+  ctlList.SetExtendedStyle( LVS_EX_HEADERDRAGDROP | LVS_EX_FULLROWSELECT | 
+                            LVS_EX_GRIDLINES | LVS_EX_SUBITEMIMAGES );
 
 // Create the Image List
-	BOOL crRes = m_ctlImage.Create( IDB_ROOMS_IMAGELIST, 16, 0, RGB(255,0,255));
-m_ctlImage.SetBkColor(GetSysColor(COLOR_WINDOW));
-	if( !crRes )
-		AfxMessageBox( _T("CRoomsView::OnCreate(): Image List Create Error.") );
-	ctlList.SetImageList( &m_ctlImage, LVSIL_SMALL );
-	
-	int res;
-	VERIFY( (res=ctlList.InsertColumn( 0, _T("RoomID"), LVCFMT_RIGHT, 60 )) >= 0 );
-	res=ctlList.InsertColumn( 1, _T("Path"),		LVCFMT_LEFT,  300 );
-	res=ctlList.InsertColumn( 2, _T("Removable"),	LVCFMT_CENTER, 75 );
+  BOOL crRes = m_ctlImage.Create( IDB_ROOMS_IMAGELIST, 16, 0, RGB(255,0,255));
+  m_ctlImage.SetBkColor(GetSysColor(COLOR_WINDOW));
+  if( !crRes )
+    AfxMessageBox( _T("CRoomsView::OnCreate(): Image List Create Error.") );
+  ctlList.SetImageList( &m_ctlImage, LVSIL_SMALL );
+  
+  int res;
+  VERIFY( (res=ctlList.InsertColumn( 0, _T("RoomID"), LVCFMT_RIGHT, 60 )) >= 0 );
+  res=ctlList.InsertColumn( 1, _T("Path"),		LVCFMT_LEFT,  300 );
+  res=ctlList.InsertColumn( 2, _T("Removable"),	LVCFMT_CENTER, 75 );
   res=ctlList.InsertColumn( 3, _T("Limit, MB"),	LVCFMT_RIGHT,  70 );
-	res=ctlList.InsertColumn( 4, _T("Free, KB"),	LVCFMT_RIGHT,  70 );
-	res=ctlList.InsertColumn( 5, _T("Compression"), LVCFMT_CENTER, 85 );	// (6)
+  res=ctlList.InsertColumn( 4, _T("Free, KB"),	LVCFMT_RIGHT,  70 );
+  res=ctlList.InsertColumn( 5, _T("Compression"), LVCFMT_CENTER, 85 );	// (6)
 
-	return 0;
+  return 0;
 }
 
 
 void CRoomsView::ShowRoomList()
 {
-	CListCtrl& ctlList = GetListCtrl();
+  CListCtrl& ctlList = GetListCtrl();
 
 // Generate Column Headers
-	EraseList();
+  EraseList();
 
-	for( POSITION pos=g_TheArchive.m_Rooms.GetHeadPosition(); pos != NULL; )
-	{
-		CRoom *pCurRoom = g_TheArchive.m_Rooms.GetNext(pos);
+  for( POSITION pos=g_TheArchive.m_Rooms.GetHeadPosition(); pos != NULL; )
+  {
+    CRoom *pCurRoom = g_TheArchive.m_Rooms.GetNext(pos);
 /*
-		int nImageIndex = imgOk;
-		if( pCurRoom->m_bRemovable )
-			nImageIndex = imgRemovable;
-		if( pCurRoom->m_nSpaceFree == -1 )
-			nImageIndex = imgUnavailable;
+    int nImageIndex = imgOk;
+    if( pCurRoom->m_bRemovable )
+      nImageIndex = imgRemovable;
+    if( pCurRoom->m_nSpaceFree == -1 )
+      nImageIndex = imgUnavailable;
 */
-		CString sTmp;
-		LV_ITEM lvItem;
-		lvItem.mask = LVIF_TEXT;
-		lvItem.iItem = ctlList.GetItemCount();	// Insert at the end
-		lvItem.iSubItem = 0;
-		sTmp.Format( _T("%d"), pCurRoom->m_nRoomID );
-		LPCTSTR strItem = sTmp;
-		lvItem.pszText = (LPTSTR)strItem;
-		lvItem.mask |= LVIF_IMAGE;
-		lvItem.iImage = -1;
+    CString sTmp;
+    LV_ITEM lvItem;
+    lvItem.mask = LVIF_TEXT;
+    lvItem.iItem = ctlList.GetItemCount();	// Insert at the end
+    lvItem.iSubItem = 0;
+    sTmp.Format( _T("%d"), pCurRoom->m_nRoomID );
+    LPCTSTR strItem = sTmp;
+    lvItem.pszText = (LPTSTR)strItem;
+    lvItem.mask |= LVIF_IMAGE;
+    lvItem.iImage = -1;
 
-/* Так почему-то не получилось, пришлось делать SetItemState
-	// State M
-		lvItem.mask |= LVIF_STATE;
-		lvItem.stateMask = LVIS_STATEIMAGEMASK;	// Bits 12-15
-		lvItem.state = INDEXTOSTATEIMAGEMASK(nState);
+/* Somehow, this didn't work. I did SetItemState instead
+  // State M
+    lvItem.mask |= LVIF_STATE;
+    lvItem.stateMask = LVIS_STATEIMAGEMASK;	// Bits 12-15
+    lvItem.state = INDEXTOSTATEIMAGEMASK(nState);
 */
-		int newItem = ctlList.InsertItem(&lvItem);
-		DBG_UNREFERENCED_LOCAL_VARIABLE(newItem);
+    int newItem = ctlList.InsertItem(&lvItem);
+    DBG_UNREFERENCED_LOCAL_VARIABLE(newItem);
 
-		BOOL res = ctlList.SetItemState( newItem, INDEXTOSTATEIMAGEMASK( 11/*TO DO nState*/),
-			LVIS_STATEIMAGEMASK );//INDEXTOSTATEIMAGEMASK(nState) );
-		DBG_UNREFERENCED_LOCAL_VARIABLE(res);
+    BOOL res = ctlList.SetItemState( newItem, INDEXTOSTATEIMAGEMASK( 11/*TO DO nState*/),
+      LVIS_STATEIMAGEMASK );//INDEXTOSTATEIMAGEMASK(nState) );
+    DBG_UNREFERENCED_LOCAL_VARIABLE(res);
 
-		SetSubItems( newItem, pCurRoom );
-	}
+    SetSubItems( newItem, pCurRoom );
+  }
 }
 
 /* (4)
 void CRoomsView::EraseList()
 {
-	CListCtrl& ctlList = GetListCtrl();
-	ctlList.DeleteAllItems();
+  CListCtrl& ctlList = GetListCtrl();
+  ctlList.DeleteAllItems();
 //	while( ctlList.DeleteColumn(0) );
 }
 */
@@ -215,99 +199,99 @@ void CRoomsView::EraseList()
 void CRoomsView::OnRoomProperties() 
 {
 // Get selected Room
-	int nSelItem;
-	CRoom *pSelRoom = GetSelectedRoom( nSelItem );
-	if( pSelRoom )
-	{
+  int nSelItem;
+  CRoom *pSelRoom = GetSelectedRoom( nSelItem );
+  if( pSelRoom )
+  {
 /* (1) Was
-	CListCtrl& ctlList = GetListCtrl();
-	int nItem = -1;
-	POSITION pos = ctlList.GetFirstSelectedItemPosition();
-	CRoom *pSelRoom = NULL;
-	if (pos != NULL)	//	There can not be multiselection!  X while (pos)
-	{
-		nItem = ctlList.GetNextSelectedItem(pos);
-		CString txt = ctlList.GetItemText( nItem, m_nIDColumn );
-		int nID = atoi( txt );
-		pSelRoom = theArchive.m_Rooms.RoomFind( nID ); */
-	
-	// Show Dialog
-		int result = CRoomPropDialog::Edit( pSelRoom );	/* (5) Was:
-		CRoomPropDialog dlg;
-		dlg.m_Limit = pSelRoom->m_nSizeLimit >> 10;	// From Bytes to KB
-		int result = dlg.DoModal();
-		if (result==IDOK)
-		{
-		// Store changed data
-			pSelRoom->m_nSizeLimit = dlg.m_Limit << 10;	// From KB to Bytes
+  CListCtrl& ctlList = GetListCtrl();
+  int nItem = -1;
+  POSITION pos = ctlList.GetFirstSelectedItemPosition();
+  CRoom *pSelRoom = NULL;
+  if (pos != NULL)	//	There can not be multiselection!  X while (pos)
+  {
+    nItem = ctlList.GetNextSelectedItem(pos);
+    CString txt = ctlList.GetItemText( nItem, m_nIDColumn );
+    int nID = atoi( txt );
+    pSelRoom = theArchive.m_Rooms.RoomFind( nID ); */
+  
+  // Show Dialog
+    int result = CRoomPropDialog::Edit( pSelRoom );	/* (5) Was:
+    CRoomPropDialog dlg;
+    dlg.m_Limit = pSelRoom->m_nSizeLimit >> 10;	// From Bytes to KB
+    int result = dlg.DoModal();
+    if (result==IDOK)
+    {
+    // Store changed data
+      pSelRoom->m_nSizeLimit = dlg.m_Limit << 10;	// From KB to Bytes
 
-			if( ! pTheDB->/*(3) theDB.* /RoomUpdate( pSelRoom ) )
-				result = IDCANCEL;
-		}*/
-		if (result==IDOK)
-		// Update List Control
-			SetSubItems( nSelItem, pSelRoom );
-	}
+      if( ! pTheDB->/*(3) theDB.* /RoomUpdate( pSelRoom ) )
+        result = IDCANCEL;
+    }*/
+    if (result==IDOK)
+    // Update List Control
+      SetSubItems( nSelItem, pSelRoom );
+  }
 }
 
 
 bool CRoomsView::SetSubItems( int nItem, CRoom* pRoom )
 {
-	bool bOk = true;
-	CString sTmp;
-	LPCTSTR strItem;
-	CListCtrl& ctlList = GetListCtrl();
-	LV_ITEM lvItem;
+  bool bOk = true;
+  CString sTmp;
+  LPCTSTR strItem;
+  CListCtrl& ctlList = GetListCtrl();
+  LV_ITEM lvItem;
 
-	lvItem.mask = LVIF_TEXT;
-	lvItem.iItem = nItem;
-	lvItem.iSubItem = 1;	// LATER: Moving Columns?
-	sTmp = pRoom->getFullName();
-	strItem = sTmp;
-	lvItem.pszText = (LPTSTR)(strItem);
-	if( ! ctlList.SetItem(&lvItem) )
-		bOk = false;
+  lvItem.mask = LVIF_TEXT;
+  lvItem.iItem = nItem;
+  lvItem.iSubItem = 1;	// LATER: Moving Columns?
+  sTmp = pRoom->getFullName();
+  strItem = sTmp;
+  lvItem.pszText = (LPTSTR)(strItem);
+  if( ! ctlList.SetItem(&lvItem) )
+    bOk = false;
 
-	lvItem.mask = LVIF_TEXT /* (2) | LVIF_IMAGE */;
-	if( pRoom->m_bRemovable )
-	{
-		lvItem.mask |= LVIF_IMAGE;	// (2)
-		lvItem.iImage = imgRemovable;
-	}
-	lvItem.iItem = nItem;
-	lvItem.iSubItem = 2;
-	strItem = pRoom->m_bRemovable ? _T("Yes") : _T("No");
-	lvItem.pszText = (LPTSTR)(strItem);
-	if( ! ctlList.SetItem(&lvItem) )
-		bOk = false;
+  lvItem.mask = LVIF_TEXT /* (2) | LVIF_IMAGE */;
+  if( pRoom->m_bRemovable )
+  {
+    lvItem.mask |= LVIF_IMAGE;	// (2)
+    lvItem.iImage = imgRemovable;
+  }
+  lvItem.iItem = nItem;
+  lvItem.iSubItem = 2;
+  strItem = pRoom->m_bRemovable ? _T("Yes") : _T("No");
+  lvItem.pszText = (LPTSTR)(strItem);
+  if( ! ctlList.SetItem(&lvItem) )
+    bOk = false;
 
-	lvItem.mask = LVIF_TEXT;
-	lvItem.iItem = nItem;
-	lvItem.iSubItem = 3;
-	sTmp.Format( _T("%d"), pRoom->m_sizeLimit >> 20 /* To Megabytes */ );
-	strItem = sTmp;
-	lvItem.pszText = (LPTSTR)(strItem);
-	if( ! ctlList.SetItem(&lvItem) )
-		bOk = false;
+  lvItem.mask = LVIF_TEXT;
+  lvItem.iItem = nItem;
+  lvItem.iSubItem = 3;
+  sTmp.Format( _T("%d"), pRoom->m_sizeLimit >> 20 /* To Megabytes */ );
+  strItem = sTmp;
+  lvItem.pszText = (LPTSTR)(strItem);
+  if( ! ctlList.SetItem(&lvItem) )
+    bOk = false;
 
-	lvItem.mask = LVIF_TEXT /* (2) | LVIF_IMAGE */;
-	lvItem.iItem = nItem;
-	lvItem.iSubItem = 4;
-	if( pRoom->m_nDiskSpaceFree == -1 )
-	{
-		lvItem.mask |= LVIF_IMAGE;	// (2)
-		lvItem.iImage = imgUnavailable;
-		sTmp = "Unavailable";
-	}
-	else
-	{
-		// (2) lvItem.iImage = -1;	// Nothing
-		sTmp.Format( _T("%d"), pRoom->m_nDiskSpaceFree >> 10 );
-	}
-	strItem = sTmp;
-	lvItem.pszText = (LPTSTR)(strItem);
-	if( ! ctlList.SetItem(&lvItem) )
-		bOk = false;
+  lvItem.mask = LVIF_TEXT /* (2) | LVIF_IMAGE */;
+  lvItem.iItem = nItem;
+  lvItem.iSubItem = 4;
+  if( pRoom->m_nDiskSpaceFree == -1 )
+  {
+    lvItem.mask |= LVIF_IMAGE;	// (2)
+    lvItem.iImage = imgUnavailable;
+    sTmp = "Unavailable";
+  }
+  else
+  {
+    // (2) lvItem.iImage = -1;	// Nothing
+    sTmp.Format( _T("%d"), pRoom->m_nDiskSpaceFree >> 10 );
+  }
+  strItem = sTmp;
+  lvItem.pszText = (LPTSTR)(strItem);
+  if( ! ctlList.SetItem(&lvItem) )
+    bOk = false;
 
 // (6) "Compression mode" column
   lvItem.mask = LVIF_TEXT;
@@ -338,24 +322,24 @@ bool CRoomsView::SetSubItems( int nItem, CRoom* pRoom )
 // Create a new Archive Room
 void CRoomsView::OnRoomNew() 
 {
-	if( g_TheArchive.m_Rooms.RoomCreate() )
-		ShowRoomList();
+  if( g_TheArchive.m_Rooms.RoomCreate() )
+    ShowRoomList();
 }
 
 
 void CRoomsView::OnUpdateRoomProperties(CCmdUI* pCmdUI) 
 {
 // Enable it if anything is selected
-	CListCtrl& ctlList = GetListCtrl();
-	POSITION pos = ctlList.GetFirstSelectedItemPosition();
-	pCmdUI->Enable( pos != NULL );
+  CListCtrl& ctlList = GetListCtrl();
+  POSITION pos = ctlList.GetFirstSelectedItemPosition();
+  pCmdUI->Enable( pos != NULL );
 }
 
 
 void CRoomsView::OnKeyDown(NMHDR* pNMHDR, LRESULT* pResult) 
 {
-	MyOnKeyDown( pNMHDR );
-	*pResult = 0;
+  MyOnKeyDown( pNMHDR );
+  *pResult = 0;
 }
 
 
@@ -364,11 +348,11 @@ void CRoomsView::OnKeyDown(NMHDR* pNMHDR, LRESULT* pResult)
 //  If nothing is selected  - IDR_ROOMS_POPUP.
 void CRoomsView::ShowPopupMenu( int nItemSel, CPoint curPoint )
 {
-	if( nItemSel == -1 )
-	// Nothing is selected
-		DoPopupMenu( IDR_ROOMS_POPUP, curPoint );
-	else
-		DoPopupMenu( IDR_ROOM_POPUP, curPoint );
+  if( nItemSel == -1 )
+  // Nothing is selected
+    DoPopupMenu( IDR_ROOMS_POPUP, curPoint );
+  else
+    DoPopupMenu( IDR_ROOM_POPUP, curPoint );
 }
 
 
@@ -379,43 +363,43 @@ void CRoomsView::OnRoomDelete()
 
 // TO DO: Glass Cursor
 
-	if( AfxMessageBox( _T("Are you sure to delete this Room\n")
-					   _T("and all Copies stored within this Room?"), MB_YESNO ) == IDYES )
-	{
-		int nSelItem;
-		CRoom* pSelRoom = GetSelectedRoom( nSelItem );
+  if( AfxMessageBox( _T("Are you sure to delete this Room\n")
+                     _T("and all Copies stored within this Room?"), MB_YESNO ) == IDYES )
+  {
+    int nSelItem;
+    CRoom* pSelRoom = GetSelectedRoom( nSelItem );
 
-		g_TheArchive.m_Rooms.Delete( pSelRoom );
+    g_TheArchive.m_Rooms.Delete( pSelRoom );
 
-	// Update display
-		ShowRoomList();
-		((CMainFrame*)AfxGetMainWnd())->CopiesWindowUpdate();
-	}
+  // Update display
+    ShowRoomList();
+    ((CMainFrame*)AfxGetMainWnd())->CopiesWindowUpdate();
+  }
 }
 
 
 // (1) Get selected Room
 CRoom* CRoomsView::GetSelectedRoom( int& nSelectedItem )
 {
-	CListCtrl& ctlList = GetListCtrl();
-	nSelectedItem = -1;
-	POSITION pos = ctlList.GetFirstSelectedItemPosition();
-	CRoom *pSelRoom = NULL;
-	if (pos != NULL)	//	There can not be multiselection!
-	{
-		nSelectedItem = ctlList.GetNextSelectedItem( pos );
-		CString txt = ctlList.GetItemText( nSelectedItem, m_nIDColumn );
-		int nID = _ttoi(txt); // atoi( txt );
-		pSelRoom = g_TheArchive.m_Rooms.RoomFind( nID );
-	}
-	return pSelRoom;
+  CListCtrl& ctlList = GetListCtrl();
+  nSelectedItem = -1;
+  POSITION pos = ctlList.GetFirstSelectedItemPosition();
+  CRoom *pSelRoom = NULL;
+  if (pos != NULL)	//	There can not be multiselection!
+  {
+    nSelectedItem = ctlList.GetNextSelectedItem( pos );
+    CString txt = ctlList.GetItemText( nSelectedItem, m_nIDColumn );
+    int nID = _ttoi(txt); // atoi( txt );
+    pSelRoom = g_TheArchive.m_Rooms.RoomFind( nID );
+  }
+  return pSelRoom;
 }
-	
+  
 
 void CRoomsView::OnUpdateRoomDelete(CCmdUI* pCmdUI) 
 {
 // Enable it if anything is selected
-	CListCtrl& ctlList = GetListCtrl();
-	POSITION pos = ctlList.GetFirstSelectedItemPosition();
-	pCmdUI->Enable( pos != NULL );
+  CListCtrl& ctlList = GetListCtrl();
+  POSITION pos = ctlList.GetFirstSelectedItemPosition();
+  pCmdUI->Enable( pos != NULL );
 }
--- CRoomsView.h
+++ CRoomsView.h
@@ -1,76 +1,66 @@
 // DiskArcher.
-// Interface of the CRoomsView class.
-// (C) Marat Mirgaleev, 2001-2003.
+// CRoomsView class - shows Rooms.
+// (C) Marat Mirgaleev, 2001-2014.
 // Modifications:
-//	(4) 16.01.2003. EraseList() moved into the base class.
+//  (4) 16.01.2003. EraseList() moved into the base class.
 //=====================================================================
 
-#if !defined(AFX_RoomsView_H__B23BE5F0_C975_423F_A345_9E50125FE4DC__INCLUDED_)
-#define AFX_RoomsView_H__B23BE5F0_C975_423F_A345_9E50125FE4DC__INCLUDED_
-
-#if _MSC_VER > 1000
 #pragma once
-#endif // _MSC_VER > 1000
-
 
 #include "CMyListView.h"
 
-class CRoom;	 // M
-class CRoomsDoc; // M
+class CRoom;
+class CRoomsDoc;
+
 
 class CRoomsView : public CMyListView
 {
 protected: // create from serialization only
-	CRoomsView();
-	DECLARE_DYNCREATE(CRoomsView)
+  CRoomsView();
+  DECLARE_DYNCREATE(CRoomsView)
 
-// Attributes
 public:
-	CRoomsDoc* GetDocument();
-	CImageList m_ctlImage; // M
+  CRoomsDoc* GetDocument();
+  CImageList m_ctlImage;
 
-// Operations
 public:
 
 // Overrides
-	// ClassWizard generated virtual function overrides
-	//{{AFX_VIRTUAL(CRoomsView)
-	public:
-	virtual void OnDraw(CDC* pDC);  // overridden to draw this view
-	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
-	protected:
-	virtual void OnInitialUpdate(); // called first time after construct
-	//}}AFX_VIRTUAL
+  // ClassWizard generated virtual function overrides
+  //{{AFX_VIRTUAL(CRoomsView)
+  public:
+  virtual void OnDraw(CDC* pDC);  // overridden to draw this view
+  virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+  protected:
+  virtual void OnInitialUpdate(); // called first time after construct
+  //}}AFX_VIRTUAL
 
-// Implementation
 public:
-	CRoom* GetSelectedRoom( int& nSelectedItem );
-// (4)	void EraseList();
-	void ShowRoomList();
-	virtual ~CRoomsView();
+  CRoom* GetSelectedRoom( int& nSelectedItem );
+// (4) void EraseList();
+  void ShowRoomList();
 #ifdef _DEBUG
-	virtual void AssertValid() const;
-	virtual void Dump(CDumpContext& dc) const;
+  virtual void AssertValid() const;
+  virtual void Dump(CDumpContext& dc) const;
 #endif
 
 protected:
-	void OnRightClick( NMHDR *pNotifyStruct, LRESULT *result );
-	int  m_nIDColumn;
+  void OnRightClick( NMHDR *pNotifyStruct, LRESULT *result );
+  int  m_nIDColumn;
 
-// Generated message map functions
 protected:
-	void ShowPopupMenu( int nItemSel, CPoint curPoint );
-	bool SetSubItems( int nItem, CRoom* pRoom );
-	//{{AFX_MSG(CRoomsView)
-	afx_msg int OnCreate(LPCREATESTRUCT lpCreateStruct);
-	afx_msg void OnRoomProperties();
-	afx_msg void OnRoomNew();
-	afx_msg void OnUpdateRoomProperties(CCmdUI* pCmdUI);
-	afx_msg void OnKeyDown(NMHDR* pNMHDR, LRESULT* pResult);
-	afx_msg void OnRoomDelete();
-	afx_msg void OnUpdateRoomDelete(CCmdUI* pCmdUI);
-	//}}AFX_MSG
-	DECLARE_MESSAGE_MAP()
+  void ShowPopupMenu( int nItemSel, CPoint curPoint );
+  bool SetSubItems( int nItem, CRoom* pRoom );
+  //{{AFX_MSG(CRoomsView)
+  afx_msg int OnCreate(LPCREATESTRUCT lpCreateStruct);
+  afx_msg void OnRoomProperties();
+  afx_msg void OnRoomNew();
+  afx_msg void OnUpdateRoomProperties(CCmdUI* pCmdUI);
+  afx_msg void OnKeyDown(NMHDR* pNMHDR, LRESULT* pResult);
+  afx_msg void OnRoomDelete();
+  afx_msg void OnUpdateRoomDelete(CCmdUI* pCmdUI);
+  //}}AFX_MSG
+  DECLARE_MESSAGE_MAP()
 };
 
 #ifndef _DEBUG  // debug version in RoomsView.cpp
@@ -82,5 +72,3 @@ inline CRoomsDoc* CRoomsView::GetDocument()
 
 //{{AFX_INSERT_LOCATION}}
 // Microsoft Visual C++ will insert additional declarations immediately before the previous line.
-
-#endif // !defined(AFX_RoomsView_H__B23BE5F0_C975_423F_A345_9E50125FE4DC__INCLUDED_)
